b9a38e3d227fb7b7983d6956fa659c52f4c37b3f
carla2807 <cm90_mdp@hotmail.com>
2022-02-18 15:09:24 -0300

    last changes

--- src/validate/validations.js
+++ src/validate/validations.js
@@ -1,16 +1,22 @@
-export const required = (value) => (value ? undefined : 'Required');
+export const required = (value) => (value ? undefined : 'Required ❗');
 
-export const number = (value) =>
-  !Number(value) ? 'Must be numeric only' : undefined;
+export const number = (value) => (!Number(value) ? 'Only numbers' : undefined);
 
 export const minValue = (value) =>
-  value < 17 ? 'Age must be minimun of 17 years' : undefined;
+  value < 17 ? 'Minimum age is 17' : undefined;
 
 export const trim = (value) =>
   !/(^\s)|(\s$)/.test(value) ? undefined : 'Invalid format';
 
 export const string = (value) =>
-  /^[A-Za-z\s]+$/.test(value) ? undefined : 'Must be letters only';
+  /^[A-Za-z\s]+$/.test(value) ? undefined : 'Only letters';
+
+export const usernameFormat = (value) =>
+  /^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){2,22}[a-zA-Z0-9]$/.test(
+    value
+  )
+    ? undefined
+    : 'Invalid format';
 
 export const composeValidators =
   (...validators) =>
